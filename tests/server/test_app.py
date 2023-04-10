b9b3bfc375a7556a0940cccdb5158945571ded78
Marcelo Vilela <mlvilela@gmail.com>
2023-04-10 17:56:50 +0100

    implement chat route

--- tests/server/test_app.py
+++ tests/server/test_app.py
@@ -1,12 +1,25 @@
 import unittest
 import json
+from unittest.mock import patch
+
 from src.server.app import app
+from src.server.language_model.gpt_client import GptClient
 
 
 class TestApp(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.client = app.test_client()
+
+    @patch.object(GptClient, 'chat')
+    def test_chat(self, mock_chat):
+        mock_chat.return_value = "Hello, how can I assist you?"
+        response = self.client.post('/chat', data={'message': 'Hi there!'})
+        data = response.get_json()
+
+        mock_chat.assert_called_once_with('Hi there!')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['response'], "Hello, how can I assist you?")
 
     def test_list_commands(self):
         response = self.client.get('/command')
