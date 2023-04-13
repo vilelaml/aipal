cb947c39612c0c4a4d220d3c2e888cc335ad740f
Marcelo Vilela <mlvilela@gmail.com>
2023-04-13 11:01:27 +0100

    small prompt improvement

--- src/client/console/prompt.py
+++ src/client/console/prompt.py
@@ -1,9 +1,9 @@
 class Prompt:
     @classmethod
     def user(cls):
-        print("\t\033[1;32mUSER:\033[0m\n\t\t", end="")
+        print("\033[1;32mUSER:\033[0m\n> ", end="")
         return input()
 
     @classmethod
     def ai(cls, content):
-        print(f"\t\033[1;31mAI:\033[0m\n\t\t{content}\n")
+        print(f"\033[1;31mAI:\033[0m\n{content}\n")
