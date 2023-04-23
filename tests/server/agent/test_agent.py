635723feed075a2efbe6074840ed0f3744b974f5
Marcelo Vilela <mlvilela@gmail.com>
2023-04-23 11:45:46 +0100

    clean up tests

--- tests/server/agent/test_agent.py
+++ tests/server/agent/test_agent.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-from tempfile import NamedTemporaryFile, TemporaryDirectory
+from tempfile import TemporaryDirectory
 from unittest.mock import patch
 
 from src.server.agent.agent import Agent
@@ -19,6 +19,7 @@ class TestAgent(unittest.TestCase):
         self.agent2 = Agent(name="Bob", goal="Complete the project")
         self.agent1.datastore = datastore
         self.agent2.datastore = datastore
+        self.agent1.save()
 
     def tearDown(self):
         self.file_patcher.stop()
@@ -27,27 +28,23 @@ class TestAgent(unittest.TestCase):
             os.rmdir(self.temp_dir.name)
 
     def test_save(self):
-        self.agent1.save()
         self.agent2.save()
         datastore = YamlDatastore(self.file_name)
         self.assertEqual(len(datastore.data), 2)
 
     def test_get(self):
-        self.agent1.save()
         self.agent2.save()
         agent = Agent.get(2)
         self.assertEqual(agent.name, "Bob")
         self.assertEqual(agent.goal, "Complete the project")
 
     def test_update(self):
-        self.agent1.save()
         self.agent1.goal = "Finish the race"
         self.agent1.update()
         agent = Agent.get(1)
         self.assertEqual(agent.goal, "Finish the race")
 
     def test_delete(self):
-        self.agent1.save()
         self.agent2.save()
         self.agent2.delete()
         datastore = YamlDatastore(self.file_name)
@@ -56,5 +53,4 @@ class TestAgent(unittest.TestCase):
             Agent.get(2)
 
     def test___str__(self):
-        self.agent1.save()
         self.assertEqual("1 - Alice: Win the race (Alice)", self.agent1.__str__())
