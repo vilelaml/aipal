e4609f48bf70485098f7b423e1e4f4091c4c4dbc
Marcelo Vilela <mlvilela@gmail.com>
2023-04-23 11:44:21 +0100

    add yaml record

--- src/server/agent/agent.py
+++ src/server/agent/agent.py
@@ -8,24 +8,5 @@ class Agent(YamlRecord):
         self.goal = goal
         self.memory_file = memory_file or name
 
-    # @classmethod
-    # def get(cls, record_id):
-    #     record = cls.datastore.get_record_by_id('Agent', record_id)
-    #     del record["type"]
-    #     return cls(**record)
-    #
-    # def save(self) -> None:
-    #     if not hasattr(self, "id") or self.id is None:
-    #         self.id = self.datastore.get_next_id()
-    #     self.datastore.create_record(str(self.id),
-    #                                  {"type": "Agent", "id": self.id, "name": self.name, "goal": self.goal})
-    #
-    # def update(self) -> None:
-    #     self.datastore.update_record(str(self.id),
-    #                                  {"type": "Agent", "id": self.id, "name": self.name, "goal": self.goal})
-    #
-    # def delete(self) -> None:
-    #     self.datastore.delete_record(str(self.id))
-    #
     def __str__(self) -> str:
         return f"{self.id} - {self.name}: {self.goal} ({self.memory_file})"
